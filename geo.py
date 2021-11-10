4fef311b415c0fc65d108324aac08553a6413a36
Winnie <woow@oregonstate.edu>
2021-11-10 11:18:53 -0800

    json output

--- geo.py
+++ geo.py
@@ -1,7 +1,3 @@
-# CS361 Assignment 5: MVP Microservices
-# Author: Winnie Woo
-# Date: 11/10/2021
-
 from geopy.geocoders import Nominatim
 import json
 geolocator = Nominatim(user_agent="TestGeolocator")
@@ -10,16 +6,15 @@ lat = float(input("Input a Latitude: "))
 long = float(input("Input a Longitude: "))
 
 location = geolocator.reverse(f"{lat}, {long}")
-#location = geolocator.reverse(f"44.569754, -123.296360")
+#location = geolocator.reverse(f"44.0332925, 122.9930591")
 
 # List of keys to remove from the location output
 outputClean = ["place_id", "licence", "osm_type", "osm_id", "display_name", "boundingbox"]
 for nameToRemove in outputClean:
     del location.raw[nameToRemove]
 
-# Convert the raw location to JSOON
-jsonOutput = json.dumps(str(location.raw))
+output = str(location.raw).encode("utf8")
 
-# Write the data to a file as jsoon
-with open("LocationOutput.json", "w") as f:
-    f.write(jsonOutput)
+# Write the data to a file as json
+with open("locationOutput.json", "wb") as f:
+    f.write(output)
