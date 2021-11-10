e7038ecebbb30ff83f6a322584df6eeaa536630a
Winnie <woow@oregonstate.edu>
2021-11-10 11:26:23 -0800

    reformatting json

--- geo.py
+++ geo.py
@@ -6,15 +6,15 @@ lat = float(input("Input a Latitude: "))
 long = float(input("Input a Longitude: "))
 
 location = geolocator.reverse(f"{lat}, {long}")
-#location = geolocator.reverse(f"44.0332925, 122.9930591")
+#location = geolocator.reverse(f"44.0332925, -122.9930591")
 
 # List of keys to remove from the location output
 outputClean = ["place_id", "licence", "osm_type", "osm_id", "display_name", "boundingbox"]
 for nameToRemove in outputClean:
     del location.raw[nameToRemove]
 
-output = str(location.raw).encode("utf8")
+output = str(location.raw).replace("'", "\"")
 
 # Write the data to a file as json
-with open("locationOutput.json", "wb") as f:
+with open("locationOutput.json", "w") as f:
     f.write(output)
