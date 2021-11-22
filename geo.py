b71ce5813d6e30f2b3add38722469673d1eb511f
Winnie Woo <86391366+winniiew@users.noreply.github.com>
2021-11-22 10:46:30 -0800

    Update geo.py

--- geo.py
+++ geo.py
@@ -6,7 +6,6 @@ lat = float(input("Input a Latitude: "))
 long = float(input("Input a Longitude: "))
 
 location = geolocator.reverse(f"{lat}, {long}")
-#location = geolocator.reverse(f"44.0332925, -122.9930591")
 
 # List of keys to remove from the location output
 outputClean = ["place_id", "licence", "osm_type", "osm_id", "display_name", "boundingbox"]
